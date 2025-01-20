41dd8b8f7935b64d5129211e17f8c75706f5bd6a
Miguel Perez Martinez <miguel.perez@neuraptic.ai>
2025-01-20 18:34:34 +0100

    Refactor MapComponent to implement Leaflet map with current location marker and improve layout styling

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,30 +1,23 @@
-// components/MapComponent.js
-"use client";
+"use client"; // If using Next.js 13+ with App Router
 
-import { Grid2 as Grid } from "@mui/material";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
 const MapComponent = ({ lat, lon }) => {
   return (
-    <Grid size={12} sx={{ display: "flex", flexDirection: "column" }}>
-      {/* <MapContainer
-        center={[lat, lon]}
-        zoom={13}
-        style={{ width: "100%", aspectRatio: 1 }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[lat, lon]}></Marker>
-      </MapContainer> */}
-      <h2 style={{ marginBottom: -15 }}>Curent position:</h2>
-      <h4 style={{ marginBottom: -15 }}>Latitude: {lat}</h4>
-      <h4> Longitude: {lon}</h4>
-    </Grid>
+    <MapContainer
+      center={[lat, lon]}
+      zoom={20}
+      style={{ height: "150px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[lat, lon]}>
+        <Popup>Your Location</Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
